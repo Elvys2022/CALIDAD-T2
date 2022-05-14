b755cf7245ed238a3c399591f168806e0759616c
Elvys2022 <gutierrezcoronadoelvisjoker@gmail.com>
2022-05-13 22:52:16 -0500

    first commit

---
+++ CalidadT2/Repositorio/BibliotecaRepository.cs
@@ -0,0 +1,25 @@
+﻿using CalidadT2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalidadT2.Repositorio
+{
+    public interface IBibliotecaRepository
+    {
+      
+    }
+    public class BibliotecaRepository : IBibliotecaRepository
+    {
+        private readonly AppBibliotecaContext app;
+
+        public BibliotecaRepository(AppBibliotecaContext app)
+        {
+            this.app = app;
+        }
+
+        
+    }
+}
